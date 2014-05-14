ed6a027bfc44fa0da3e151fbdcfa4cbf6b8a978f
Connor <CShocrylas@gmail.com>
2014-05-14 09:40:37 -0300

    Custom Fields Addition and Example Template

---
+++ wp-content/themes/farming/page-templates/ecosystem_services.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template Name: Ecosystem Services
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="main-content" class="main-content">
+
+<?php
+	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
+		// Include the featured content template.
+		get_template_part( 'featured-content' );
+	}
+?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<?php
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+				endwhile;
+			?>
+
+				<div class="testimonial">
+				<p><?php the_field('testimonials'); ?></p>
+				<p class="testimonial_author"><?php the_field('testimonial_author'); ?></p>
+
+
+
+				</div>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+</div><!-- #main-content -->
+
+<?php
+get_sidebar();
+get_footer();
