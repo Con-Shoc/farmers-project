0fae96901f7bd053e33085ddf75c3828c220d0c7
sahlal <sahool.90@gmail.com>
2014-06-30 19:01:24 -0300

    archive-page.php fix

--- wp-content/themes/farming/page-templates/archive-page.php
+++ wp-content/themes/farming/page-templates/archive-page.php
@@ -20,106 +20,124 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content button-page-margins" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-					// Include the page content template.
+	
 
-					get_template_part( 'content', 'page' );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
-				endwhile;
-			?>
-			
-		
-		<?php query_posts('post_type=resource_link'); 
-
-			$categories = array();
-
-
-			// Fill an array of all distinct cateogories for links and resources
-			while(have_posts()): the_post();
-
-				$current_catObject = get_the_category();
-				
-				$current = $current_catObject[0]->cat_name;
-
-				// Checks to make sure duplicate entries are not included. This prevents too many accordions from being created.
-				if(!in_array($current, $categories)):
-
-					$result = array_push($categories, $current);
-
-				endif;
-
-			endwhile;
-
-			// Cycle through each category and and create an accordion.
-			foreach($categories as $category) {
-			
-
-		?>
-
-        <div class="accordion">
-            <dl>				
-              <dt><a class='accordionTitle' <?php echo 'style="background-color:';	echo '#48B749';	echo ' ;" ';?> onMouseOver="this.style.backgroundColor='<?php  echo '#299546';?>'"	onMouseOut="this.style.backgroundColor='<?php  echo '#48B749';?>'"> <?php echo $category ?></a></dt>
-
-              <dd class="accordionItem accordionItemCollapsed">
-                
-                <div class="row">
-                <?php 
-
-                	query_posts('post_type=resource_link');
-
-                	while ( have_posts() ): the_post();
-
-						$current_category = get_the_category();
+						<?php
 					
-                		// Checks if the current post belongs to the current accordion category.
-	                	if($current_category[0]->cat_name == $category):
+
+						
+						// array to hold all of the categories (even repeated)
+						$allCatArray = array();
+
+						// query to be used in a loop through custom posts. exclusive for (resource_link)
+						$args = array( 'post_type' => 'resource_link', 'posts_per_page' => -1);
+						$loop = new WP_Query( $args );
+
+								// while loop to iterate through every post using previous query
+								// purpose of this loop is to get all the categories
+								while ( $loop->have_posts() ) : $loop->the_post();
+											
+
+											// echo get_post_meta($post->ID,'link_title',true);
+											// echo "<br>";
+
+								// storing all categories in an array which is returned by default
+								$all= get_the_category( );
+								
+								// storing each category in a different array
+								array_push($allCatArray, $all[0]->cat_name);
+
+								endwhile;
+
+								wp_reset_query();
+
+								// fetching only unique categories to eliminate redanduncies
+								$uniqueCatArray = array_unique($allCatArray);
+								array_multisort($uniqueCatArray);
+								array_filter($uniqueCatArray);
+								
 
 
-							$link_title = get_field('link_title');
+								for ($i=0; $i < count($uniqueCatArray); $i++) { 
+							
 
-							$link_address = get_field('link_address');
-
-							$link_description = get_field('link_description');
-					?>
-
-
-					<div class="col-lg-6">
-
-		                 <div class="panel panel-success">
-						  <a class="link_title" href='<?php echo $link_address ?>'>
-						  <div class="panel-heading">
-						    <h3 class="panel-title">
-						    <?php echo $link_title ?></h3>
-						  </div>
-						 
-						  <div class="panel-body">
-						  	<!-- <p class="link_description_title">Link Description</p> -->
-						    <?php echo $link_description;?>
-						  </div>
-						</div>
-						 </a>
-
-					</div>
+											$args = array( 'post_type' => 'resource_link', 'posts_per_page' => -1 ,'category_name' => $uniqueCatArray[$i]);
+											$loop = new WP_Query( $args );
+											?>
+							
 
 
-					<?php
-                		endif;
+													<div class="accordion">
+											            <dl>				
+											              <dt><a class='accordionTitle' <?php echo 'style="background-color:';	echo '#48B749';	echo ' ;" ';?> onMouseOver="this.style.backgroundColor='<?php  echo '#299546';?>'"	onMouseOut="this.style.backgroundColor='<?php  echo '#48B749';?>'"> <?php echo $uniqueCatArray[$i] ?></a></dt>
 
-                	endwhile; ?>
-				</div>
- 
-              </dd>
+											              <dd class="accordionItem accordionItemCollapsed">
+											                
+											                <div class="row">
+       
 
-            </dl>
 
-            <?php
-             } ?>
+											<?php
+												while ( $loop->have_posts() ) : $loop->the_post();
+												
+												?>
+												
+													<div class="col-lg-6">
+
+										                 <div class="panel panel-success">
+														  <a class="link_title" href='<?php echo get_post_meta($post->ID,'link_address',true); ?>'>
+														  <div class="panel-heading">
+														    <h3 class="panel-title">
+														    <?php echo get_post_meta($post->ID,'link_title',true); ?></h3>
+														  </div>
+														 
+														  <div class="panel-body">
+														  	
+														    <?php echo get_post_meta($post->ID,'link_description',true);?>
+														  </div>
+														</div>
+														 </a>
+
+													</div>
+
+
+												<?php
+												endwhile;
+												wp_reset_query();
+												?>
+
+													</div>
+												 
+												              </dd>
+
+												            </dl>
+
+
+												<?php
+								}
+
+						?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
           </div>
       
