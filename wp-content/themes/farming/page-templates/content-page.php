0b0e66a790adefaf499f0d0ee8dd0e33d77a6fe3
Andrew Sampson <andrewziac91@gmail.com>
2014-07-10 12:48:59 -0300

    Kick ass code fix to fix the accordion menus with more than 2 accordions.

--- wp-content/themes/farming/page-templates/content-page.php
+++ wp-content/themes/farming/page-templates/content-page.php
@@ -47,7 +47,6 @@ get_header(); ?>
 			 
 			            $output = "<li style='background-image: url(". $bgimg ."); background-size:100%; max-height: 325px; min-height: 270px;'>\n";
 			            $output .= "  <div class='slide-text'>\n";
-			            $output .= "  <h2>". $row['slide_heading'] ."</h2>\n";
 			            $output .= "  " . $row['slide_text'];
 			            $output .= "  </div>\n";
 			            $output .= "</li>\r\n\n";
@@ -226,27 +225,27 @@ get_header(); ?>
 			 	// loop through the rows of data
 			    while ( have_rows('accordion_section') ) : the_row();
 
-				// read sub fields into variables
-				$accordion_title = get_sub_field('accordion_title');
-				$accordion_content = get_sub_field('accordion_content');
+					// read sub fields into variables
+					$accordion_title = get_sub_field('accordion_title');
+					$accordion_content = get_sub_field('accordion_content');
 
-				$accordion_content_image = get_sub_field('accordion_content_image');
-        ?>
-              <dt><a class='accordionTitle' <?php echo 'style="background-color:';	the_sub_field('accordion_color');	echo ' ;" ';?> onMouseOver="this.style.backgroundColor='<?php  echo hex2rgbDark(get_sub_field('accordion_color'));?>'"	onMouseOut="this.style.backgroundColor='<?php the_sub_field('accordion_color'); ?>'" ><?php echo $accordion_title ?></a></dt>
+					$accordion_content_image = get_sub_field('accordion_content_image');
+	        		?>
+	              	<dt><a class='accordionTitle' <?php echo 'style="background-color:';	the_sub_field('accordion_color');	echo ' ;" ';?> onMouseOver="this.style.backgroundColor='<?php  echo hex2rgbDark(get_sub_field('accordion_color'));?>'"	onMouseOut="this.style.backgroundColor='<?php the_sub_field('accordion_color'); ?>'" ><?php echo $accordion_title ?></a></dt>
 
-              <dd class="accordionItem accordionItemCollapsed">
-                <?php echo $accordion_content ?>
-                <?php if($accordion_content_image != ''): ?>
-                	<img class="accordionimage" src="<?php echo $accordion_content_image['url']; ?>" alt="<?php echo $accordion_content_image['alt']; ?>" />
-                <?php endif; ?>
+	              	<dd class="accordionItem accordionItemCollapsed">
+	                <?php echo $accordion_content ?>
+                	<?php if($accordion_content_image != ''): ?>
+                		<img class="accordionimage" src="<?php echo $accordion_content_image['url']; ?>" alt="<?php echo $accordion_content_image['alt']; ?>" />
+                	<?php endif; ?>
     				<!-- sub repeater loop for buttons within accordions -->
 			                <?php
 
-			                if( have_rows('accordion_button') ):
+	                if( have_rows('accordion_button') ):
 
-			                	?>
-			                <?php
-			                while( have_rows('accordion_button') ): the_row();
+	                	?>
+	                <?php
+		                while( have_rows('accordion_button') ): the_row();
 			                $accordion_button_image = get_sub_field('accordion_button_image');
 			                $blankImage=__DIR__.'/../images/blank.png';
 			                ?>
@@ -274,108 +273,105 @@ get_header(); ?>
 			           		 </div>
 
 
-			                <?php
-			                endwhile;
-			                endif;
-								?>
+		                <?php
+		                endwhile;
+	                endif;?>
 			                
-							<?php
-							// Start nested accordion, almost identical to parent accordion
-			                if( have_rows('nested_accordion') ):
+					<?php
+					// Start nested accordion, almost identical to parent accordion
+	                if( have_rows('nested_accordion') ):
 			                	?>
-			                <div class="nested_accordion">
-							<dl>
-								<?php
-			                	while (have_rows('nested_accordion')): the_row();
+		                <div class="nested_accordion">
+						<dl>
+						<?php
+	                	while (have_rows('nested_accordion')): the_row();
 
-			                		// read sub fields into variables
-									$nested_accordion_title = get_sub_field('nested_accordion_title');
-									$nested_accordion_content = get_sub_field('nested_accordion_content');
+	                		// read sub fields into variables
+							$nested_accordion_title = get_sub_field('nested_accordion_title');
+							$nested_accordion_content = get_sub_field('nested_accordion_content');
 
-									$nested_accordion_image = get_sub_field('nested_accordion_image');
-					        ?>
-					              <dt><a class='accordionTitle' <?php echo 'style="background-color:';	the_sub_field('nested_accordion_color');	echo ' ;" ';?> onMouseOver="this.style.backgroundColor='<?php  echo hex2rgbDark(get_sub_field('nested_accordion_color'));?>'"	onMouseOut="this.style.backgroundColor='<?php the_sub_field('nested_accordion_color'); ?>'" ><?php echo $nested_accordion_title ?></a></dt>
+							$nested_accordion_image = get_sub_field('nested_accordion_image');
+			        		?>
+			              	<dt><a class='accordionTitle' <?php echo 'style="background-color:';	the_sub_field('nested_accordion_color');	echo ' ;" ';?> onMouseOver="this.style.backgroundColor='<?php  echo hex2rgbDark(get_sub_field('nested_accordion_color'));?>'"	onMouseOut="this.style.backgroundColor='<?php the_sub_field('nested_accordion_color'); ?>'" ><?php echo $nested_accordion_title ?></a></dt>
 
-					              <dd class="accordionItem accordionItemCollapsed">
-					                <?php echo $nested_accordion_content ?>
-					                <?php if($nested_accordion_image != ''): ?>
-					                	<img class="accordionimage" src="<?php echo $nested_accordion_image['url']; ?>" alt="<?php echo $nested_accordion_image['alt']; ?>" />
-					                <?php endif; ?>
-
-
-
-					                				<!-- sub repeater loop for buttons within accordions -->
-											                <?php
-
-											                if( have_rows('nested_accordion_button') ):
-
-											                	?>
-											                <?php
-											                while( have_rows('nested_accordion_button') ): the_row();
-											                $nested_button_image = get_sub_field('nested_button_image');
-											                $blankImage=__DIR__.'/../images/blank.png';
-											                ?>
+			              	<dd class="accordionItem accordionItemCollapsed">
+			                <?php echo $nested_accordion_content ?>
+					        <?php if($nested_accordion_image != ''): ?>
+			                	<img class="accordionimage" src="<?php echo $nested_accordion_image['url']; ?>" alt="<?php echo $nested_accordion_image['alt']; ?>" />
+			                <?php endif; ?>
 
 
-											                <div class='menu_button' <?php echo 'style="background-color:';	the_sub_field('nested_button_color');	echo ' ;" ';?> onMouseOver="this.style.backgroundColor='<?php  echo hex2rgbDark(get_sub_field('nested_button_color'));?>'"	onMouseOut="this.style.backgroundColor='<?php the_sub_field('nested_button_color'); ?>'">
-											                
-											                	<a class="menu_link" href="<?php the_sub_field('nested_button_link'); ?>">
-											                		
-											                		<?php 
-											                		if(empty($nested_button_image)){
-											                		?>
-											                		<img class="button_image" src="./wp-content/themes/farming/images/blank.png" alt="<?php echo 'no image'; ?>" />
-											                		<?php
-											                		}
+
+    				<!-- sub repeater loop for buttons within accordions -->
+			                <?php
+
+			                if( have_rows('nested_accordion_button') ):
+			                	?>
+			                <?php
+				                while( have_rows('nested_accordion_button') ): the_row();
+					                $nested_button_image = get_sub_field('nested_button_image');
+					                $blankImage=__DIR__.'/../images/blank.png';
+					                ?>
 
 
-											                		else{
-											                		?>
-											                		<img class="button_image" src="<?php echo $nested_button_image['url']; ?>" alt="<?php echo $nested_button_image['alt']; ?>" />
-											                		<?php } ?>	
-											                			<div class="button_text">
-											                				<!-- display a sub field value -->
-																        	<p class="button_name">
-																        	<?php the_sub_field('nested_button_name'); ?>
-																        	</p>
+					                <div class='menu_button' <?php echo 'style="background-color:';	the_sub_field('nested_button_color');	echo ' ;" ';?> onMouseOver="this.style.backgroundColor='<?php  echo hex2rgbDark(get_sub_field('nested_button_color'));?>'"	onMouseOut="this.style.backgroundColor='<?php the_sub_field('nested_button_color'); ?>'">
+					                
+				                	<a class="menu_link" href="<?php the_sub_field('nested_button_link'); ?>">
+												                		
+			                		<?php 
+			                		if(empty($nested_button_image)){
+				                		?>
+				                		<img class="button_image" src="./wp-content/themes/farming/images/blank.png" alt="<?php echo 'no image'; ?>" />
+				                		<?php
+				                		}
 
-																        	</br>
 
-																        	<p class="button_blurb">
-																       		<?php the_sub_field('nested_button_blurb'); ?> 
-																       		</p>
-											                			</div>
-											                	</a>
+			                		else{
+			                			?>
+			                			<img class="button_image" src="<?php echo $nested_button_image['url']; ?>" alt="<?php echo $nested_button_image['alt']; ?>" />
+		                			<?php } ?>	
+		                			<div class="button_text">
+		                				<!-- display a sub field value -->
+							        	<p class="button_name">
+							        	<?php the_sub_field('nested_button_name'); ?>
+							        	</p>
 
-											           		 </div>
+							        	</br>
 
-							           		 <?php
-							                endwhile;
-							                endif;
+							        	<p class="button_blurb">
+							       		<?php the_sub_field('nested_button_blurb'); ?> 
+							       		</p>
+		                			</div>
+				                	</a>
 
-							               ?>
+				           		 	</div>
 
-			              </dd>
+			           		 	
+				           		 <?php
+			                	endwhile;
+		                	endif;
+
+		               ?>
+
+		             	 </dd>
 			              <?php
-			          endwhile;
-
-			          endif; ?>
-			            </dl>
-			          </div>
-
-
-                
-              </dd>
-              <?php
-          endwhile;
+			        	endwhile; ?>
+			        	</dl>
+			        	</div>
+		        	<?php endif; ?>
+              	</dd>
+              	<?php
+          		endwhile;
 				?>
-            </dl>
-          </div>
-          <!-- Accordion Stops -->
-          <?php
-          endif;
-          ?>
+          
+	        <?php
+	        endif;
+	        ?>
 
+        	</dl>
+        </div>
+
+          <!-- Accordion Stops -->
 		<?php if( have_rows('button') ):
 
 			while( have_rows('button') ): the_row(); ?>
