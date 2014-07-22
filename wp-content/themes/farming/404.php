f0aad09a835f75bd432bb6bfe5e41d6d551406a6
Connor <CShocrylas@gmail.com>
2014-07-22 17:07:23 -0300

    Neo changes

--- wp-content/themes/farming/404.php
+++ wp-content/themes/farming/404.php
@@ -13,15 +13,30 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
-			<div class='not-found'>
 			
-				<div class="test">
-				<h1> Whhhoooooooaaaaaa </h1>
-				<img src='http://localhost/farming/wp-content/uploads/2014/07/matrix.jpg' />
-				Easy man, that page is... like... non-existent...
-				</div>
 			
+				<div class="not-found" style="background: no-repeat center url(./wp-content/uploads/2014/07/matrix.jpg);'">
+					<div class="neo-text">
+						<span class="neo-arrow genericon genericon-next"></span>
+						<h1> Whhhoooooooaaaaaa </h1>
+						<p>
+							Easy man, that page is... like... non-existent...
+						</p>
+					</div>
 				</div>
+
+		 <div class="error_result_option_1">
+			Since you couldnt find your page, why not try searching for it?
+		 	<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+				<label>
+				<div class="no_result_search">
+					<input type="search" class="no_result_search-field" placeholder="Search For..." value="" name="s" title="Search for:" />
+					<input type="submit" class="no_result_search_submit" name="Submit" value="Search Site" />
+				</div>
+				</label>
+			</form>
+		</div>
+			
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
