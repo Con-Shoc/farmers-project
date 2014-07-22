7a62ac69925e7a70160b5cc07117a0df8e07aad5
Connor <CShocrylas@gmail.com>
2014-07-22 12:52:33 -0300

    Test

--- wp-content/themes/farming/content-none.php
+++ wp-content/themes/farming/content-none.php
@@ -8,15 +8,30 @@
  */
 ?>
 
-<header class="page-header">
-	<h1 class="page-title"><?php _e( 'Nothing Found', 'twentyfourteen' ); ?></h1>
-</header>
+<div class="page-content button-page-margins">
+	<div class="no_result_header"> 
+	<span class=" no_result_warning genericon genericon-warning"></span>
+	<div>
+	<p class="no_result_text">Sorry bro! No Results found! </p>
+	<p class="no_result_frown">:(</p>
+	</div>
 
-<div class="page-content">
-	<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+	</div
 
-	<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentyfourteen' ), admin_url( 'post-new.php' ) ); ?></p>
+	<!-- <?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?> -->
 
+	<!-- <p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentyfourteen' ), admin_url( 'post-new.php' ) ); ?></p> -->
+	 <div class="no_result_option_1">
+	 Well since we couldn't find your item, wanna try again?
+	 	<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+			<label>
+			<div class="no_result_search">
+				<span class="screen-reader-text">Search for:</span>
+				<input type="search" class="search-field" placeholder="Search For..." value="" name="s" title="Search for:" />
+				<input type="submit" class="search_submit" name="Submit" value="Search Site" />
+			</div>
+			</label>
+		</form>
 	<?php elseif ( is_search() ) : ?>
 
 	<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentyfourteen' ); ?></p>
