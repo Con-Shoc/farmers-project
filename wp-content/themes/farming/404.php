5b04d41bb1505853681ec8adcc68afe5853d5dde
Connor <CShocrylas@gmail.com>
2014-07-22 16:23:22 -0300

    More Changes again

--- wp-content/themes/farming/404.php
+++ wp-content/themes/farming/404.php
@@ -14,21 +14,14 @@ get_header(); ?>
 		<div id="content" class="site-content" role="main">
 
 			<div class='not-found'>
-				<header class="page-header">
-					<h1> Whhhoooooooaaaaaa </h1>
-				</header>
-				<img src='http://localhost/farming/wp-content/uploads/2014/07/matrix.jpg' />
 			
-				<div class="page-content">
-
-					<br />
-
-					<h3><?php _e( 'Easy man, that page is... like... non-existent...', 'twentyfourteen' ); ?></h3>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</div>
-
+				<div class="test">
+				<h1> Whhhoooooooaaaaaa </h1>
+				<img src='http://localhost/farming/wp-content/uploads/2014/07/matrix.jpg' />
+				Easy man, that page is... like... non-existent...
+				</div>
+			
+				</div>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
